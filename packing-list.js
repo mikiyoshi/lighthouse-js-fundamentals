18f116a483b0bd965c962001e02463f776eae9eb
mikiyoshi <mikiyoshi.kokura@gmail.com>
2021-10-09 07:13:07 +0000

    The result here will be exactly the same as before

--- packing-list.js
+++ packing-list.js
@@ -7,7 +7,12 @@ for (let i = 0; i < packingList.length; i++) {
 packingList[3];
 
 
-
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
 
 
 
