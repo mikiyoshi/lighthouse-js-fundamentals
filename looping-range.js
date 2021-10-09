d8bc886539d1e08c314a7aa743fbd96ce1634547
mikiyoshi <mikiyoshi.kokura@gmail.com>
2021-10-09 19:37:56 +0000

    this file updated for loop range challenge

---
+++ looping-range.js
@@ -0,0 +1,13 @@
+const range = function(start, end, step) {
+  const newArray = [];
+  if ( start !== false && end !== false && step > 0 ) {
+    for(let i = start; i <= end; i = i + step) {
+      newArray.push(i);
+    }
+  }
+  return newArray;
+}
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
+console.log(range(0, 10, -5));
