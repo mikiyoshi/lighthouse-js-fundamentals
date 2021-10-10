63dfed73a569cf6c40c3d7e3c0cb8a9e9cbd4933
mikiyoshi <mikiyoshi.kokura@gmail.com>
2021-10-10 16:02:28 +0000

    comment update

--- loopy-lighthouse-2.js
+++ loopy-lighthouse-2.js
@@ -3,10 +3,6 @@ function loopyLighthouse(range, multiples, words) {
   // console.log(multiples[1]);
   // console.log(words);
   for (let i = range[0]; i <= range[1]; i++ ) {
-      // console.log(i);
-      // if (i % 5) {
-      //   console.log(words[0]);
-      // }
     if (!(i % multiples[0]) && !(i % multiples[1])) {
       console.log(words[0] + words[1]);
     } else if (!(i % multiples[0])) {
